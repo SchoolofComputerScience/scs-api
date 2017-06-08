7c515c5a5df80603242242ee99ef217673eb5ea3
Shane Eckel <shaneckel@gmail.com>
2017-06-08 17:12:04 -0400

    updated news limit

--- data.js
+++ data.js
@@ -281,7 +281,7 @@ module.exports = {
     return pris.api(prismicApi).then(function(api) {
       return api.query(
         pris.Predicates.at('document.type', 'news'),
-        { pageSize : 10, page: 1, orderings : '[my.news.publish_date desc]' }
+        { pageSize : 20, page: 1, orderings : '[my.news.publish_date desc]' }
       )
     })
     .then((res) => res.results);
