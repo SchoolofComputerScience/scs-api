58e6c77bf0955e80f97e82ccfefe57bbf9c51dd4
Clyde Nelson <clizzy124life@aol.com>
2018-08-14 11:30:01 -0400

    Adding Events

--- src/types/events.js
+++ src/types/events.js
@@ -1,7 +1,7 @@
 import {
   GraphQLObjectType,
   GraphQLList,
-  GraphQLString,
+  GraphQLString
 } from 'graphql';
 
 export const EventsType = new GraphQLObjectType({
@@ -9,75 +9,15 @@ export const EventsType = new GraphQLObjectType({
   description: 'Events feed from Prismic.io',
   fields: () => ({
     id: { type: GraphQLString },
-    uid: {
-      type: GraphQLString,
-      resolve: (parent, args) => parent.uid
-    },
-    room: {
-      type: GraphQLString,
-      resolve: (parent, args) => {
-        let room = ''
-        if(parent.data['events.room']){
-          room = parent.data['events.room'].value[0].text
-        }
-        return parent.data ? room : parent.room || ''
-      }
-    },
-    building: {
-      type: GraphQLString,
-      resolve: (parent, args) => parent.data['events.location'] ? parent.data['events.location'].value[0].text : ''
-    },
-    title: {
-      type: GraphQLString,
-      resolve: (parent, args) => parent.data['events.title'] ? parent.data['events.title'].value[0].text : ''
-    },
-    abstract: {
-      type: GraphQLString,
-      resolve: (parent, args) => parent.data['events.starttime'] ? parent.data['events.starttime'].value[0].text : ''
-    },
-    startDate: {
-      type: GraphQLString,
-      resolve: (parent, args) => parent.data['events.starttime'] ? parent.data['events.starttime'].value : ''
-    },
-    endDate: {
-      type: GraphQLString,
-      resolve: (parent, args) => parent.data['events.endtime'] ? parent.data['events.endtime'].value : ''
-    },
-    talkTitle: {
-      type: GraphQLString,
-      resolve: (parent, args) => parent.data['events.subtitle'] ? parent.data['events.subtitle'].value[0].text : ''
-    },
-    speakerName: {
-      type: GraphQLString,
-      resolve: (parent, args) => parent.data['events.speakerName'] ? parent.data['events.speakerName'].value[0].text : ''
-    },
-    speakerCompany: {
-      type: GraphQLString,
-      resolve: (parent, args) => parent.data['events.speakerCompany'] ? parent.data['events.speakerCompany'].value[0].text : ''
-    },
-    speakerCompanyTeam: {
-      type: GraphQLString,
-      resolve: (parent, args) => parent.data['events.speakerCompanyTeam'] ? parent.data['events.speakerCompanyTeam'].value[0].text : ''
-    },
-    type: {
-      type: GraphQLString,
-      resolve: (parent, args) => parent.data['events.eventtype'] ? parent.data['events.eventtype'].value : ''
-    },
-    tags: {
-      type: new GraphQLList(GraphQLString),
-      resolve: (parent, args) => parent.tags
-    },
-    eventUrl: {
-      type: GraphQLString,
-      resolve: (parent, args) => parent.data['events.eventurl'] ? parent.data['events.eventurl'].value.url : ''
-    },
-    poster: {
-      type: GraphQLString,
-      resolve: (parent, args) => parent.data['events.posterurlpdf'] ? parent.data['events.posterurlpdf'].value.url : ''
-    },
-    posterText: {
-      type: GraphQLString,
-      resolve: (parent, args) => parent.posterText ? parent.posterText : ''
-    }
+    room: { type: GraphQLString },
+    building: { type: GraphQLString },
+    name: { type: GraphQLString },
+    date: { type: GraphQLString },
+    speakerName: { type: GraphQLString },
+    // type: { type: GraphQLString },
+    // tags: { type: new GraphQLList(GraphQLString) },
+    eventUrl: { type: GraphQLString }
+    // poster: { type: GraphQLString },
+    // posterText: { type: GraphQLString }
   })
 })
